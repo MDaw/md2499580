ce6226c0798d153225a61ad0631f091ccc6f1bcb
MDaw <MarwahDaw@gmail.com>
2014-01-15 14:03:11 -0800

    Signed-off-by: MDaw <MarwahDaw@gmail.com>

---
+++ Assignment 1/Savitch Ch 1 Probv10/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Marwah Dawoody
+ *
+ * Created on January 10, 2014, 9:15 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    //Declare the Char variable
+    char letter;
+    //Input the letter
+    cout<<"What letter would you like to use"
+            <<"to output a capitial C?"<<endl;
+    cout<<"Hit enter/return after the letter"<<endl;
+    cin>>letter;
+    //Output the Capital C
+    cout<<" "<<letter<<" "<<letter<<" "<<letter<<endl;
+    cout<<" "<<letter<<" "<<" "<<" "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<" "<<" "<<" "<<letter<<endl;
+    cout<<" "<<letter<<" "<<letter<<" "<<letter<<endl;
+
+    return 0;
+}
+
